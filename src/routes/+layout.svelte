476cad8cbf58831772b283adf32fe981f902e622
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-02 17:17:53 -0500

    wip header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Header from './header.svelte'
+
+	import '../app.css'
+	import 'sanitize.css'
+</script>
+
+<div class="layout">
+	<Header />
+	
+	<main>
+		<slot />
+	</main>
+</div>
