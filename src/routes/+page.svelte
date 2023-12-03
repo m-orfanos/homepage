275b4574e013c1b8664f1e0d1dd3d048d736638b
Miltiadis Orfanos <morfanos@tuta.io>
2023-12-03 18:59:57 -0500

    init project (#1)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<article class="about">
+  <h1>Hi, I'm Miltiadis!</h1>
+
+  <p>
+    I'm a software developer based in Eastern Canada. I have formal training in both
+    <a href="https://en.wikipedia.org/wiki/Building_engineer">building engineering</a>
+    and computer science. Most of my experience in the former is in project management, 
+    specifically large civil engineering projects.
+  </p>
+
+  <p>
+    I've been working professionally in the technology sector since 2012, and unprofessionally since
+    2004 where I learned programming to solve engineering/math-related problems. You can find out
+    more about my profesional career on
+    <a href="https:// www.linkedin.com/in/morfanos">Linkedin</a>.
+  </p>
+
+  <p>
+    In my free time, I still find a joy in coding and learning new technologies, mostly through practical projects.
+    However, I also believe it's important to be well-rounded, which is why I dedicate as much time, if not more,
+    reading about various topics beyond tech. When I'm not learning something new, I like to go for runs, long walks, or
+    play video games.
+  </p>
+
+  <p>
+    Finally, I am easiest to reach on <a href="https://twitter.com/morf4nos">Twitter/X</a>.
+  </p>
+</article>
+
+<style lang="scss">
+  .about {
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+</style>
---
+++ src/routes/header.svelte
@@ -0,0 +1,71 @@
+<script>
+  import Github from 'svelte-bootstrap-icons/lib/Github.svelte';
+  import Linkedin from 'svelte-bootstrap-icons/lib/Linkedin.svelte';
+  import Twitter from 'svelte-bootstrap-icons/lib/TwitterX.svelte';
+
+  import Toggle from './toggle.svelte';
+</script>
+
+<header class="header">
+  <a class="header-info" href="/" title="Home">
+    <h1 class="header-title">Miltiadis Orfanos</h1>
+    <div class="header-tagline">Software Developer</div>
+  </a>
+
+  <nav class="header-nav">
+    <a href="https://github.com/m-orfanos" title="Github">
+      <Github width="24" height="24" />
+    </a>
+    <a href="https://www.linkedin.com/in/morfanos" title="Linkedin">
+      <Linkedin width="24" height="24" />
+    </a>
+    <a href="https://twitter.com/morf4nos" title="Twitter">
+      <Twitter width="24" height="24" />
+    </a>
+    <Toggle />
+  </nav>
+</header>
+
+<style lang="scss">
+  .header {
+    align-items: center;
+    background: var(--mantle);
+    display: grid;
+    grid-template-areas: 'logo nav';
+    grid-template-columns: 1fr 1fr;
+    justify-content: space-between;
+    padding: var(--gap);
+
+    a {
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+      &:visited {
+        color: inherit;
+      }
+    }
+
+    .header-info {
+      .header-title {
+        font-size: 1.2em;
+        margin: 0.15em 0;
+      }
+      .header-tagline {
+        font-size: 0.8em;
+      }
+    }
+
+    .header-nav {
+      display: flex;
+      gap: 0.8em;
+      justify-content: end;
+      margin: auto var(--gap);
+      a {
+        &:hover {
+          color: var(--blue);
+        }
+      }
+    }
+  }
+</style>
